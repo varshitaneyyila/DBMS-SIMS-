79356ed681365109af691cabd7055918e870a810
Varshita Neyyila <varshitaneyyila@gmail.com>
2026-05-27 15:52:26 +0530

    Add files via upload

---
+++ Backend/seed.py
@@ -0,0 +1,9 @@
+﻿from app import create_app
+from app.bootstrap import initialize_database
+
+app = create_app()
+
+
+with app.app_context():
+    initialize_database(seed_admin=True)
+    print("Seed data created")
